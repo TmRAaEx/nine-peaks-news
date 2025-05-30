f89c852b4f334faa1a411acf69cd8d7576d7675b
TmRAaEx <tmr.aaex@gmail.com>
2025-05-30 14:03:04 +0200

    Created Params type for the api routes

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,10 +1,11 @@
 // app/api/[slug]/route.ts
-import { NextResponse } from 'next/server';
+import Params from "@/types/Params";
+import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    const { id } = params;
-    // Fetch or generate data based on slug
-    const data = { message: `Data for slug: ${id}` };
+export async function GET(request: Request, {params}: Params) {
+  const { id } = await params;
+  // Fetch or generate data based on slug
+  const data = { message: `Data for slug: ${id}` };
 
-    return NextResponse.json(data);
+  return NextResponse.json(data);
 }
