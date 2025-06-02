107f9f43264398b7b7fe3bfd2fd760fea6c1b1e3
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 15:50:31 +0200

    added free status

--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -1,28 +1,34 @@
-import mongoose, {Document, Model, Schema} from 'mongoose';
-import { IUser } from './User';
-import { ITier } from './Tier';
+import mongoose, { Document, Model, Schema } from "mongoose";
+import { IUser } from "./User";
+import { ITier } from "./Tier";
 
-export interface IPayment extends Document {    
-    user_id: mongoose.Types.ObjectId | IUser;
-    tier_id: mongoose.Types.ObjectId | ITier;
-    payment_date: Date;
-    due_date: Date;
-    status?: number;
-    stripe_ref: string;
+export interface IPayment extends Document {
+  user_id: mongoose.Types.ObjectId | IUser;
+  tier_id: mongoose.Types.ObjectId | ITier;
+  payment_date: Date;
+  due_date: Date;
+  status: "paid" | "pending" | "failed" | "free";
+  stripe_ref?: string | null;
 }
 
-const PaymentSchema = new Schema<IPayment>({
-    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    tier_id: { type: Schema.Types.ObjectId, ref: 'Tier', required: true },
-    payment_date: {type: Date, required: true},
-    due_date: {type: Date, required: true},
-    status: { type: Number, required: false },
-    stripe_ref: {type: String, required: true }
-},
-  { timestamps: true } 
+const PaymentSchema = new Schema<IPayment>(
+  {
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    tier_id: { type: Schema.Types.ObjectId, ref: "Tier", required: true },
+    payment_date: { type: Date, required: true },
+    due_date: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["paid", "pending", "failed", "free"],
+      required: true,
+      default: "free",
+    },
+    stripe_ref: { type: String, required: false, default: null },
+  },
+  { timestamps: true }
 );
 
 const Payment: Model<IPayment> =
-  mongoose.models.Payment || mongoose.model<IPayment>('Payment', PaymentSchema);
+  mongoose.models.Payment || mongoose.model<IPayment>("Payment", PaymentSchema);
 
-export default Payment; 
+export default Payment;
