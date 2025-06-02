3821cd9dd3a7e885c797f8147ac6f0cdbeed6995
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 15:50:54 +0200

    added images and filtered out admin

--- src/lib/tiers.ts
+++ src/lib/tiers.ts
@@ -7,12 +7,14 @@ export default async function GetTiers(): Promise<ITierData[]> {
     await dbConnect();
     const tiers = await Tier.find().lean(); // plain JS objects
 
-    const returnTiers = tiers.map((tier, index) => ({
-      ...tier,
-      name: tier._id.toString(), // use ID as display name
-      price: tier.price?.toString(), // convert price to string
-      image: `/img/icons/tier${index + 1}-fill.png`,
-    }));
+    const returnTiers = tiers
+      .map((tier, index) => ({
+        ...tier,
+        name: tier._id.toString(), // use ID as display name
+        price: tier.price?.toString(), // convert price to string
+        image: `/img/icons/tier${index + 1}-fill.svg`,
+      }))
+      .filter((tier) => tier._id != "Admin");
 
     return returnTiers;
   } catch (error) {
