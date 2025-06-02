c2e7c678e72e4c807513b1d93f4eb4e5f52077cb
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 15:30:48 +0200

    added image to tiers

--- src/components/Subscriptions/SubscriptionTier.tsx
+++ src/components/Subscriptions/SubscriptionTier.tsx
@@ -7,11 +7,11 @@ export default function SubscriptionTier({ tier }: { tier: ITierData }) {
   return (
     <li className="flex flex-col justify-between text-center">
       <h1 className="font-bold text-xl">{tier.name}</h1>
-      <Image src={""} alt="" />
+      <Image src={tier.image} alt={tier._id} />
       <Link href={`/subscribe?${tier._id}`}>
-      <SecondaryButton>
-        {tier.price != "0" ? "$" + tier.price : "Free"}
-      </SecondaryButton>
+        <SecondaryButton>
+          {tier.price != "0" ? "$" + tier.price : "Free"}
+        </SecondaryButton>
       </Link>
     </li>
   );
