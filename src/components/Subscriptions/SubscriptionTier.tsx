ca221beb7d1a75bcb28a1e9e1a0979c1509ad99d
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 14:38:48 +0200

    Created subscription componttes

--- src/components/Subscriptions/SubscriptionTier.tsx
+++ src/components/Subscriptions/SubscriptionTier.tsx
@@ -1,15 +1,18 @@
 import { ITierData } from "@/interfaces/ITierData";
 import Image from "next/image";
 import SecondaryButton from "@/buttons/Secondarybutton";
+import Link from "next/link";
 
 export default function SubscriptionTier({ tier }: { tier: ITierData }) {
   return (
     <li className="flex flex-col justify-between text-center">
       <h1 className="font-bold text-xl">{tier.name}</h1>
       <Image src={""} alt="" />
+      <Link href={`/subscribe?${tier._id}`}>
       <SecondaryButton>
         {tier.price != "0" ? "$" + tier.price : "Free"}
       </SecondaryButton>
+      </Link>
     </li>
   );
 }
