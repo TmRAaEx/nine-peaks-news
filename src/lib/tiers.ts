9217d006d5bbb5e79f0fe1cfca326c5afec85900
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 13:47:28 +0200

    Made tiers not get returned as monogoose documents but raw json objects

--- src/lib/tiers.ts
+++ src/lib/tiers.ts
@@ -1,16 +1,18 @@
+import { ITierData } from "@/interfaces/ITierData";
 import dbConnect from "@/lib/ConnectDB";
-import Tier, {ITier} from "@/models/Tier";
+import Tier from "@/models/Tier";
 
 
-export default async function GetTiers(): Promise<ITier[]> {
+export default async function GetTiers(): Promise<ITierData[]> {
     try {
         await dbConnect();
         const tiers = await Tier.find().lean(); // plain JS objects
 
 
         const returnTiers = tiers.map((tier) => ({
-            ...tier,
-            name: tier._id.toString(),    // use ID as display name
+          ...tier,
+          name: tier._id.toString(), // use ID as display name
+          price: tier.price?.toString(), // convert price to string
         }));
 
         return returnTiers;
