baf13ea2f84fca795b5b1a2094cca68075cd02ef
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 12:27:11 +0200

    Created api endpoint for regestering a user

---
+++ src/lib/Authentication.ts
@@ -0,0 +1,27 @@
+import connectDB from "./ConnectDB";
+import User, { IUser } from "@/models/User";
+
+type IUserApiData = {
+  userName: IUser["userName"];
+  email: IUser["email"];
+  password: IUser["password"];
+};
+//TODO Refactor to Partial<IUser>
+export async function RegisterUser(data: IUserApiData): Promise<IUser | any> {
+  try {
+    await connectDB();
+
+    const isUser = await User.findOne({ email: data.email });
+
+    if (isUser) {
+      return { error: "User exist" };
+    }
+
+    const createdUser = await User.create(data);
+    return createdUser;
+    
+  } catch (err) {
+    console.error("[LIB Authentication Register]", err);
+    return { error: err };
+  }
+}
