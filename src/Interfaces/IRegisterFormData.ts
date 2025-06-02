909a5cdbb79140647b3adad610b0019754b7b83e
TmRAaEx <tmr.aaex@gmail.com>
2025-06-02 11:39:32 +0200

    Created basic register page for register and Interface for the form data

---
+++ src/Interfaces/IRegisterFormData.ts
@@ -0,0 +1,6 @@
+export default interface IRegisterData {
+  userName: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+}
--- src/app/authentication/register/page.tsx
+++ src/app/authentication/register/page.tsx
@@ -1,3 +1,69 @@
-export default function Register(){
-    return <>This is the register page</>
+"use client";
+
+import IRegisterData from "@/interfaces/IRegisterFormData";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+export default function Register() {
+  const [formData, setFormData] = useState<IRegisterData>({
+    userName: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value, name } = e.target;
+
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <label htmlFor="userName">username</label>
+        <input
+          type="text"
+          name="userName"
+          value={formData.userName}
+          onChange={handleChange}
+          placeholder="username"
+        />
+
+        <label htmlFor="email">email</label>
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="email"
+        />
+        <label htmlFor="password">email</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <label htmlFor="confirm_password">email</label>
+        <input
+          type="password"
+          name="confirm_password"
+          value={formData.confirm_password}
+          onChange={handleChange}
+          placeholder="confirm password"
+        />
+        <button role="submit">Register</button>
+      </form>
+    </>
+  );
 }
