a0d61910f9cc29a6792ecbb50bb0ebefbdce389c
Michelle Soderstrand <misode@hotmail.com>
2025-06-03 09:58:51 +0200

    added handelchange, fixed api endpoint

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ISignUserIn from "@/interfaces/ISignUserIn";
 import { authClient } from "@/lib/ApiClient";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Login() {
     const [formData, setFormData] = useState<ISignUserIn>({
@@ -9,26 +9,53 @@ export default function Login() {
       password: "",
     });  
     const [error, setError] = useState<string | null>(null);
-    
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
 
-        const response = await authClient.get<any>("/login", formData)
-      
-    if (response.error) {
-      setError(response.error);
-    }
-
-    console.log(response.data);
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
+
+  const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
+      try {
+        const response = await authClient.post<any>("/login", formData);
+    
+        if (response.error) {
+          setError(response.error);
+        } else {
+          console.log(response.data);
+        }
+        } catch (err: any) {
+          setError(err.message || "Login failed");
+      }
+  };
+
+
   return (
-  <>
-  <form onSubmit={handleSubmit}>
-    <label htmlFor="email">email</label>
-    <input type="email" name="email"  />
-    <label htmlFor="password">password</label>
-    <input type="password" name="password"/>
-    <button role="submit">Sign in!</button>  
-  </form>
-  </>);
+        <>
+          <form onSubmit={handleSubmit}>
+            <p>{error}</p>
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">Sign in!</button>
+          </form>
+        </>
+  );
 }
