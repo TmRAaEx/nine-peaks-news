eaafbe64f5ba29ee5f2b45a56893c615ac4cb95c
TmRAaEx <tmr.aaex@gmail.com>
2025-06-03 14:20:02 +0200

    Pages and compoentns

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import GetTiers from "@/lib/tiers";
-import SubscriptionTier from "@/components/Subscriptions/SubscriptionTier";
+import SubscriptionList from "@/components/Subscriptions/SubscriptionList";
 export default async function Upgrade() {
   const tiers = await GetTiers();
 
   return (
-    <ul className="grid grid-cols-3 gap-6">
-      {tiers.map((tier) => (
-        <SubscriptionTier key={tier._id} tier={tier} />
-      ))}
-    </ul>
+    <SubscriptionList tiers={tiers}/>
   );
 }
