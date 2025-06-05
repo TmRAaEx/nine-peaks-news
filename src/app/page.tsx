e189f52524cc485941333b34f4f93245f38ef2a1
magnus-sczel <magnus@sczel.com>
2025-06-05 13:39:06 +0300

    Added styling for darkmode, created the footer and fixed some issues with the header for Desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
             <div className="content-wrapper">
               <h1>News that travels.<br/> Everywhere</h1>
               <p>See our offers and sign up for a subscription today!</p>
-              <button className="button cta-button">CTA</button>
+              <button className="button cta-button">SEE THE PLANS</button>
             </div>
           </div>
           <div className="overlay"></div>
           <div className="image-container">
-            <img src="/img/hero-image.jpg" alt="hero image" />
+            <img src="/img/hero-image.jpg" id="heroImage" alt="hero image" />
           </div>
           <div className="circle">
             <img src="/img/icons/9-fill-lg-light.svg" alt="mambo number nine" />
