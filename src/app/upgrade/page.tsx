385d45bc4b49a9df8a25fb0515ca5a064c7908a8
tmraaex <tmr.aaex@gmail.com>
2025-06-08 02:19:49 +0200

    fixed weird typo

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GetTiers from "@/lib/tiers";
+import GetTiers from "@/lib/Tiers";
 import SubscriptionList from "@/components/Subscriptions/SubscriptionList";
 export default async function Upgrade() {
   const tiers = await GetTiers();
