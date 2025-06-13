b04e0a79a37842ef2e313e1ffd9495a3519b3e0c
TmRAaEx <tmr.aaex@gmail.com>
2025-06-13 11:18:42 +0200

    ef

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -5,17 +5,17 @@ import NoAccessModal from "./NoAccessModal";
 export default async function Articles({
   searchParams,
 }: {
-  searchParams: Record<string, string | undefined>;
+  searchParams: Promise<Record<string, string | undefined>>;
 }) {
   const { articles, error } = await ShowAllArticles();
 
-  const { noaccess, tier } = searchParams;
+  const { noaccess, tier } = await searchParams;
 
   return (
     <>
       {error && <p className="text-red-500 text-center my-4">{error}</p>}
 
-      <NoAccessModal tier={tier!} noaccess={noaccess}/>
+      <NoAccessModal tier={tier!} noaccess={noaccess} />
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 max-w-6xl mx-auto">
         {articles.map((article) => (
           <ArticleCard article={article} key={article.id} />
